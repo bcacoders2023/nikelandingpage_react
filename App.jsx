72cbec0b36594acc7060c3be5a169146a1c676c5
Yogesh  Kumar soni <153350265+bcacoders2023@users.noreply.github.com>
2024-06-01 12:36:20 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import'./App.css';
+import Navigation from './componets/Navigation';
+import Hero from './componets/Hero';
+
+const App =() =>{
+    return (
+        <div> 
+        <Navigation/>
+        <Hero/>
+        </div>
+    )
+
+};
+export default App;
---
+++ Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation =() =>{
+ return(
+    <nav className="nav">
+            <div className="logo">
+                <img src="/public/brand_logo.png" alt="logo"/>
+            </div>
+            <ul>
+                <li herf="#">Home</li>
+                <li herf="#">Location</li>
+                <li herf="#">About</li>
+                <li herf="#">Contact</li>
+            </ul>
+
+            <button>Login</button>
+        </nav>
+ )
+}
+export default Navigation;
